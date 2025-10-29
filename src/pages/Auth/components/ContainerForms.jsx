258a2f9a754506800f7bd8dd2a9d81b382c16985
kouahodavid6 <kouahodavid6@gmail.com>
2025-10-29 11:43:20 +0000

    commit

--- src/pages/Auth/components/ContainerForms.jsx
+++ src/pages/Auth/components/ContainerForms.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const ContainerForms = ({ children }) => {
     return(
-        <div className="min-h-screen bg-violet-100 flex items-center justify-center p-4">
+        <div className="min-h-screen bg-emerald-50 flex items-center justify-center p-4">
             <div className="bg-white rounded-xl shadow-lg p-8 w-full max-w-xl">
                 { children }
             </div>
